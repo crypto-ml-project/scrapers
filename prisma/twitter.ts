a21e8af0ed03e2d0e726c710b782119cff6e9847
fipso <fipso@users.noreply.github.com>
2022-12-13 17:56:55 +0100

    fix race condition; use batched insert transactions

--- prisma/twitter.ts
+++ prisma/twitter.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, SocialMediaPosts } from "@prisma/client";
 import { MessageBuilder, Webhook } from "discord-webhook-node";
 const { spawn } = require("child_process");
 const fs = require("fs-extra");
@@ -21,101 +21,46 @@ child.stderr.on("data", (data: any) => {
 });
 
 let tweetCount = 0;
-let createCount = 0;
-let duplicateCount = 0;
+//let createCount = 0;
+//let duplicateCount = 0;
+
+let chunk: any[] = [];
+let saving = false;
 
 child.stdout.on("data", async (data: any) => {
   let tmpTweets = data.toString().split(/\n/g);
-
   tmpTweets = tmpTweets.filter((tweet: any) => tweet !== "");
 
   const tweets = tmpTweets.slice(0, tmpTweets.length - 1).map((tweet: any) => {
-    tweetCount++;
     return JSON.parse(tweet);
   });
+  tweetCount += tweets.length;
 
-  for (const tweet of tweets) {
-    const user = await addUserData(tweet);
-    const post = await addPostData(tweet);
-    if (!user || !post) {
-      duplicateCount++;
-    } else {
-      createCount++;
-    }
-  }
+  chunk.push(...tweets);
 });
 
-setInterval(async () => {
-  const msg = "⚙️ Tweets Processed: " + tweetCount;
-  const createMsg = "✅ Tweets Created: " + createCount;
-  const duplicateMsg = "⚠️ Duplicate Tweets: " + duplicateCount;
-  const date = new Date();
-  console.log(
-    date.toLocaleDateString(),
-    date.toLocaleTimeString(),
-    msg,
-    createMsg,
-    duplicateMsg
-  );
-}, 1000);
+async function saveChunk() {
+  saving = true;
 
-setInterval(async () => {
-  const msg = "⚙️ Tweets Processed: " + tweetCount;
-  const createMsg = "✅ Tweets Created: " + createCount;
-  const duplicateMsg = "⚠️ Duplicate Tweets: " + duplicateCount;
-  const date = new Date();
-  hook.send(
-    `\`\`\`${date.toLocaleDateString()} ${date.toLocaleTimeString()} | ${msg} | ${createMsg} | ${duplicateMsg}\`\`\``
-  );
-  if (
-    tweetCount > 10000 &&
-    (duplicateCount > createCount * 0.9 || createCount === 0)
-  ) {
-    hook.warning("**Duplicate Tweets:**", duplicateCount.toLocaleString());
-  }
-}, 1.8e6);
-
-async function addUserData(tweet: any) {
-  const usercheck = await prisma.user.findUnique({
-    where: {
-      userId: tweet.user.username,
-    },
-  });
-
-  if (usercheck) {
-    // console.log("⚠️ User Data already exists for: " + tweet.user.username);
-    return false;
-  } else {
-    const user = await prisma.user.create({
-      data: {
+  try {
+    const txs: any = [];
+    for (let tweet of chunk) {
+      const userData = {
         userId: tweet.user.username,
         created_at: tweet.user.created,
         follower: tweet.user.followersCount,
         description: tweet.user.description,
-      },
-    });
+      }
+      const tx = prisma.user.upsert({
+        where: {
+          userId: userData.userId,
+        },
+        create: userData,
+        update: userData
+      });
+      txs.push(tx);
 
-    if (!user) {
-      hook.error("**❌ Failed to populate User Data:**", tweet.user.username);
-      return false;
-    }
-    return true;
-  }
-}
-
-async function addPostData(tweet: any) {
-  const tweetId = tweet.url.split("/")[5];
-  const postCheck = await prisma.socialMediaPosts.findUnique({
-    where: {
-      id: tweetId,
-    },
-  });
-
-  if (postCheck) {
-    return false;
-  } else {
-    const socialMediaPost = await prisma.socialMediaPosts.create({
-      data: {
+      const tweetData = {
         id: tweet.url.split("/")[5],
         content: tweet.content,
         timeStamp: tweet.date,
@@ -123,18 +68,60 @@ async function addPostData(tweet: any) {
         comments: tweet.replyCount,
         platformId: "twitter",
         tags: tweet.hashtags ? tweet.hashtags : [],
-        user: {
-          connect: {
-            userId: tweet.user.username,
-          },
+        userId: tweet.user.username
+      };
+      const tx2 = prisma.socialMediaPosts.upsert({
+        where: {
+          id: tweetData.id
         },
-      },
-    });
-
-    if (!socialMediaPost) {
-      hook.error("**❌ Failed to populate Social Media Post Data:**", tweetId);
-      return false;
+        create: tweetData,
+        update: tweetData
+      });
+      txs.push(tx2)
     }
-    return true;
+
+    chunk = [];
+
+    await prisma.$transaction(txs);
+  } catch (err) {
+    console.log(err);
   }
+
+  saving = false;
 }
+
+setInterval(async () => {
+  if (saving || !chunk.length) return;
+  await saveChunk();
+}, 100);
+
+setInterval(async () => {
+  const msg = "⚙️ Tweets Processed: " + tweetCount;
+  //const createMsg = "✅ Tweets Created: " + createCount;
+  //const duplicateMsg = "⚠️ Duplicate Tweets: " + duplicateCount;
+  const date = new Date();
+  console.log(
+    date.toLocaleDateString(),
+    date.toLocaleTimeString(),
+    msg,
+    //createMsg,
+    //duplicateMsg
+  );
+}, 1000);
+
+setInterval(async () => {
+  const msg = "⚙️ Tweets Processed: " + tweetCount;
+  //const createMsg = "✅ Tweets Created: " + createCount;
+  //const duplicateMsg = "⚠️ Duplicate Tweets: " + duplicateCount;
+  const date = new Date();
+  hook.send(
+    `\`\`\`${date.toLocaleDateString()} ${date.toLocaleTimeString()} | ${msg}\`\`\``
+    // | ${createMsg} | ${duplicateMsg}/
+  );
+  // if (
+  //   tweetCount > 10000 &&
+  //   (duplicateCount > createCount * 0.9 || createCount === 0)
+  // ) {
+  //   hook.warning("**Duplicate Tweets:**", duplicateCount.toLocaleString());
+  // }
+}, 1.8e6);
